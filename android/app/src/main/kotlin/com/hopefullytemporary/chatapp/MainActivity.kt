56942f6cc42a5127afb188bd707748ba8093658b
Akramah Faizi <akramahahmadfaizi@gmail.com>
2022-12-20 04:24:40 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/hopefullytemporary/chatapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hopefullytemporary.chatapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import 'chat_screen.dart';
+
+class HomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("Friendy Chat"),
+      ),
+          body: ChatScreen()
+    );
+  }
+}
